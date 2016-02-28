3db9568b3f5ac49d767fb3c5f222080b77a50c34
Benjamin Turner <benturn@gmail.com>
2016-02-28 13:25:06 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(name='django-amp',
-      version='1.0.a1',
+      version='1.0.a2',
       description='A small toolset for rendering Google AMP pages for Django projects',
       url='http://github.com/blturner/django-amp',
+      download_url='https://github.com/blturner/django-amp/archive/v1.0.a2.tar.gz',
       author='Benjamin Turner',
       author_email='benturn@gmail.com',
       license='MIT',
-      packages=['amp'],
+      packages=find_packages(),
       zip_safe=False)
